6c9f97edf42bb74ddbdf1b452648a8075ddf7ca0
Machado-SI <223562434+Machado-SI@users.noreply.github.com>
2025-08-24 11:10:51 -0300

    Criação da rota registro e validando dados com yup

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express();
+const dotenv = require('dotenv')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs') 
+const yup = require('yup')
 
 // Importa o arquivo db.js onde ocorre a conexão com o banco de dados
 const db = require('./db')
 
 app.use(express.json())
+dotenv.config()
+
+// Validação de campos para registro com Yup
+const JWT_SECRET = process.env.JWT_SECRET
+const registerSchema = yup.object().shape({
+    nome: yup.string().required(),
+    senha: yup.string().required().min(6)
+})
 
 // Rota para buscar todos os cursos
 app.get('/cursos', async (req, res) => {
@@ -17,6 +29,31 @@ app.get('/cursos', async (req, res) => {
     } catch (error) {
         console.error('Erro ao buscar cursos:', error)
         res.status(500).json({message: 'Erro ao buscar cursos'})
+    }
+})
+
+// Rota para registrar um novo usuário
+app.post('/register', async (req,res) => {
+    try {
+        const {nome, senha} = await registerSchema.validate(req.body, {
+            abortEarly: false
+        }) 
+        const hashedPassword = await bcrypt.hash(senha, 10)
+        const novoUsuario = await db.one(
+            'INSERT INTO usuarios (nome, senha) VALUES ($1, $2) RETURNING id, nome', [nome, hashedPassword]
+        )
+        if(!novoUsuario) {
+            return res.status(500).json({message: 'Erro ao registrar usuário'})
+        }
+        res.status(201).json(novoUsuario)
+    } catch (error) {
+        if(error.name === 'ValidationError') {
+            return res.status(400).json({
+                message: 'Erro de validação',
+                errors: error.errors
+            })
+        }
+        res.status(500).json(error.message)
     }
 })
 
