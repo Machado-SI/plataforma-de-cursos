79956a380b4192bdbed67749da22448d0fc740e5
Machado-SI <223562434+Machado-SI@users.noreply.github.com>
2025-08-24 11:49:38 -0300

    Criação da rota login e ajustando a rota register

--- server.js
+++ server.js
@@ -13,34 +13,22 @@ dotenv.config()
 
 // Validação de campos para registro com Yup
 const JWT_SECRET = process.env.JWT_SECRET
-const registerSchema = yup.object().shape({
+const validateSchema = yup.object().shape({
     nome: yup.string().required(),
-    senha: yup.string().required().min(6)
+    senha: yup.string().required().min(6),
+    type: yup.string().oneOf(['admin', 'user']).required()
 })
 
-// Rota para buscar todos os cursos
-app.get('/cursos', async (req, res) => {
-    try {
-        const cursos = await db.any('SELECT * FROM cursos')
-        if(cursos.length === 0) {
-            return res.status(404).json({message: 'Nenhum curso encontrado'})
-        }
-        res.status(200).json(cursos)
-    } catch (error) {
-        console.error('Erro ao buscar cursos:', error)
-        res.status(500).json({message: 'Erro ao buscar cursos'})
-    }
-})
 
 // Rota para registrar um novo usuário
 app.post('/register', async (req,res) => {
     try {
-        const {nome, senha} = await registerSchema.validate(req.body, {
+        const {nome, senha, type} = await validateSchema.validate(req.body, {
             abortEarly: false
         }) 
         const hashedPassword = await bcrypt.hash(senha, 10)
-        const novoUsuario = await db.one(
-            'INSERT INTO usuarios (nome, senha) VALUES ($1, $2) RETURNING id, nome', [nome, hashedPassword]
+        const novoUsuario = await db.oneOrNone(
+            'INSERT INTO usuarios (name, senha, type) VALUES ($1, $2, $3) RETURNING id, name', [nome, hashedPassword, type]
         )
         if(!novoUsuario) {
             return res.status(500).json({message: 'Erro ao registrar usuário'})
@@ -54,6 +42,55 @@ app.post('/register', async (req,res) => {
             })
         }
         res.status(500).json(error.message)
+    }
+})
+
+// Rota para login
+app.post('/login', async (req, res) => {
+    try {
+        const {nome, senha} = await validateSchema.validate(req.body, {
+            abortEarly: false
+        })
+        
+        const user = await db.oneOrNone('SELECT * FROM usuarios WHERE name = $1', [nome])
+        
+        if(!user) {
+            return res.status(401).json({message: 'Usuário não encontrado'})
+        }
+        
+        const comparePassword = await bcrypt.compare(senha, user.senha)
+        
+        if(!comparePassword) {
+            return res.status(400).json({message: 'Senha incorreta'})
+        }
+
+        // Gerar o token JWT
+        const token = jwt.sign({id: user.id, nome: user.name, type: user.type}, JWT_SECRET, {expiresIn: '1h'})
+        return res.status(200).json({ token })
+    } catch (error) {
+        if(error.name === 'ValidationError') {
+            return res.status(400).json({
+                message: 'Erro de validação',
+                errors: error.errors
+            })
+        }
+        console.error('Erro ao fazer login:', error)
+        res.status(500).json({message: 'Erro ao fazer login'})
+    }
+})
+
+
+// Rota para buscar todos os cursos
+app.get('/cursos', async (req, res) => {
+    try {
+        const cursos = await db.any('SELECT * FROM cursos')
+        if(cursos.length === 0) {
+            return res.status(404).json({message: 'Nenhum curso encontrado'})
+        }
+        res.status(200).json(cursos)
+    } catch (error) {
+        console.error('Erro ao buscar cursos:', error)
+        res.status(500).json({message: 'Erro ao buscar cursos'})
     }
 })
 
